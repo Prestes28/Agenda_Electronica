7da0146e231d4a1a3a6313ec5328f18f69d94e9d
Prestes28 <144856856+Prestes28@users.noreply.github.com>
2024-09-20 11:21:30 -0300

    Add files via upload

---
+++ src/agendaelextronicaparcial/AgendaElextronicaParcial.java
@@ -0,0 +1,18 @@
+
+package agendaelextronicaparcial;
+
+import agendaelextronicaparcial.igu.Principal;
+
+/**
+ *
+ * @author Ezequiel
+ */
+public class AgendaElextronicaParcial  {
+
+    public static void main(String[] args){
+      Principal princ = new Principal();  
+      princ.setVisible(true);
+      princ.setLocationRelativeTo(null);
+    }
+    
+}
